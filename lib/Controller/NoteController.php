99003ea1d4af0c07f9a519e9c0cf285ed79d854c
Phie <phie@phie.ovh>
2018-08-22 00:05:29 +0200

    don't crash when no metadata

--- lib/Controller/NoteController.php
+++ lib/Controller/NoteController.php
@@ -243,9 +243,17 @@
 			try {
 				 $zipFile = new \PhpZip\ZipFile();
 				 $zipFile->openFromStream($this->CarnetFolder->get($path)->fopen("r"));
-				 $array[$path] = array();
-				 $array[$path]['metadata'] = json_decode($zipFile->getEntryContents("metadata.json"));
-				 $array[$path]['shorttext'] = substr(trim(strip_tags($zipFile->getEntryContents("index.html"))),0, 100);
+                 $array[$path] = array();
+                 try{
+                    $array[$path]['metadata'] = json_decode($zipFile->getEntryContents("metadata.json"));
+                 } catch(PhpZip\Exception\ZipNotFoundEntry e){
+
+                 }
+                 try{
+                    $array[$path]['shorttext'] = substr(trim(strip_tags($zipFile->getEntryContents("index.html"))),0, 100);
+                } catch(PhpZip\Exception\ZipNotFoundEntry e){
+
+                }
 			} catch(\OCP\Files\NotFoundException $e) {
 			}
 		}
