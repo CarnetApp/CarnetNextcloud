15ca73a5d07aa15668cb78b0f47114a02e9bd25d
Phie <phie@phie.ovh>
2018-09-19 18:36:52 +0200

    refresh cache for js files in editor

--- templates/writer.php
+++ templates/writer.php
@@ -7,9 +7,18 @@ $file = str_replace("href=\"","href=\"".substr(__DIR__, strlen($_SERVER['DOCUMEN
 $file = str_replace("<!ROOTPATH>", "", $file);
 //if($_['carnet_display_fullscreen']=="yes")
 //    script("carnet","../templates/CarnetElectron/compatibility/nextcloud/reader_fullscreen");
+//filemtime (
 
+    $file = preg_replace_callback('/<script(.*?)src=\"(.*?\.js(?:\?.*?)?)"/s',function ($matches) {
+        global $currentpath;
+        return "<script".$matches[1]."src=\"".$matches[2]."?mtime=".filemtime($currentpath.$matches[2])."\"";
+    }, $file);
 $root = substr(__DIR__, strlen($_SERVER['DOCUMENT_ROOT']));
 $file = str_replace("src=\"","defer src=\"".$root."/CarnetElectron/",$file);
+
+
+
+
 echo $file;
 echo "<span style=\"display:none;\" id=\"root-url\">".$root."/CarnetElectron/</span>";
 echo "<span style=\"display:none;\" id=\"api-url\">../../../index.php/apps/carnet/</span>";
