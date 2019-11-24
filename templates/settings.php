1c2127120d91220e583b0ec51bff385e622bd0b0
Phie <phie@phie.ovh>
2019-11-24 23:15:56 +0100

    removing src for jquery

--- templates/settings.php
+++ templates/settings.php
@@ -12,8 +12,10 @@ $file = str_replace("href=\"","href=\"".$root."/CarnetElectron/",$file);
 $file = preg_replace_callback('/<script(.*?)src=\"(.*?\.js(?:\?.*?)?)"/s',function ($matches) {
     global $currentpath;
     global $appVersion;
+    global $fullscreen;
+
     if($matches[2] === "libs/jquery.min.js" AND $fullscreen === "no")
-        return "<script src=\"\"";
+        return "<script ";
     return "<script".$matches[1]."src=\"".$matches[2]."?v=".$appVersion."\"";
 }, $file);
 // token is needed to pass the csfr check
