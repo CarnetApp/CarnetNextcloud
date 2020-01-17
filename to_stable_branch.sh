2d9e119be2041621a5ae1aee84fa49c9a0a29804
Phie <phie@phie.ovh>
2020-01-17 15:55:34 +0100

    new to_stable_branch script

--- to_stable_branch.sh
+++ to_stable_branch.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-
+version=$(sed -n -e 's/.*<version>\(.*\)<\/version>.*/\1/p' appinfo/info.xml)
 git branch -d stable
 git push origin --delete stable
 git checkout master
 git checkout -b stable
 git push origin stable
+git checkout -b stable-$version
+git push origin stable-$version
 git checkout master
