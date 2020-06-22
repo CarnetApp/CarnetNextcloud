d3813758378ba0f9ffb54f8939af602141eb3f70
Phie <phie@phie.ovh>
2020-06-23 00:31:22 +0200

    adding framagit to to_stable script

--- to_stable_branch.sh
+++ to_stable_branch.sh
@@ -3,9 +3,12 @@
 version=$(sed -n -e 's/.*<version>\(.*\)<\/version>.*/\1/p' appinfo/info.xml)
 git branch -D stable
 git push origin --delete stable
+git push framagit --delete stable
 git checkout master
 git checkout -b stable
 git push origin stable
+git push framagit stable
 git checkout -b stable-$version
 git push origin stable-$version
+git push framagit stable-$version
 git checkout master
